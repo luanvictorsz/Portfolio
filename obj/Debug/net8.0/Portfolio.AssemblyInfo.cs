a721710de8dcc3a3d33b20504d16b4640a09bbd7
Luan Victor (A3TERNUS) <luanvitorlima2017@gmail.com>
2025-01-02 13:21:58 -0300

    otimizando codigo e melhorando menu hamburguer

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e89084fa47daacf4866a181294e65b9291a61024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28c38ecddb4a9cd2da096346236c6af2ecf84616")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
