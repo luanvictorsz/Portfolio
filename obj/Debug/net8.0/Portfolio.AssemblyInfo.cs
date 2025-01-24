50f66a56dc774b897d29cbadf2a91394acb343a9
Luan Victor (A3TERNUS) <luanvitorlima2017@gmail.com>
2025-01-24 17:48:01 -0300

    pagina inicial

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a721710de8dcc3a3d33b20504d16b4640a09bbd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3452c6b5cecac610fe66b4856757c675c0cd8618")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
